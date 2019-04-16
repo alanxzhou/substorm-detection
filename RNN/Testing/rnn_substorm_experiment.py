6f3066c22de1ac08082ec1deb07f375250178a01
Alan <alanzhou93@gmail.com>
2019-04-16 03:44:11 -0400

    basic gru model seems to work

--- RNN/Testing/rnn_substorm_experiment.py
+++ RNN/Testing/rnn_substorm_experiment.py
@@ -9,7 +9,7 @@ plt.style.use('ggplot')
 
 #my_path = os.path.abspath(os.path.dirname(__file__))
 #path = os.path.join(my_path, "../../data/all_stations_data.npz")
-path = "D:\substorm-detection\data\\all_stations_data_128.npz"
+path = "D:\\substorm-detection\\data\\all_stations_data_128.npz"
 data = np.load(path)
 X = data['X']
 y = data['y'][:, None]
@@ -17,15 +17,18 @@ train_test_split = .1
 train_val_split = .15
 
 # create train, val and test sets
-train, test = utils.split_data([X, y], train_test_split, random=False)
-train, val = utils.split_data(train, train_val_split, random=True)
+train, test = utils.split_data([X, y], train_test_split, random=False, rnn_format=True)
+train, val = utils.split_data(train, train_val_split, random=True, rnn_format=True)
 X_train, y_train = train
 X_val, y_val = val
 X_test, y_test = test
 
+X_train, X_val, X_test = utils.rnn_format_x([X_train, X_val, X_test])
+y_train, y_val, y_test = utils.rnn_format_y([y_train,y_val,y_test])
+
 params = {
-    'epochs', 200,
-    'batch_size', 32,
+    'epochs': 10,
+    'batch_size': 32
 }
 
 hist, mod = rnn_models.train_basic_gru(X_train, y_train, X_val, y_val, params)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import keras.backend as K
-
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 def split_data(list_of_data, split, random=True, rnn_format=False):
     """this function splits a list of equal length (first dimension) data arrays into two lists. The length of the data
@@ -20,13 +21,12 @@ def split_data(list_of_data, split, random=True, rnn_format=False):
     for data in list_of_data:
         split_a.append(data[idx[:split_idx]])
         split_b.append(data[idx[split_idx:]])
-
+    """
     if rnn_format:
         a0,a1,a2,a3 = np.shape(split_a)
-        b0,b1,_,_ = np.shape(split_b)
-        split_a = np.reshape(split_a,(a0,a1,a2*a3))
-        split_b = np.reshape(split_b,(b0,b1,a2*a3))
-
+        
+        split_a = np.reshape(split_a,(a0,a2,a1*a3))
+    """
     return split_a, split_b
 
 
@@ -42,4 +42,21 @@ def false_positive(y_true, y_pred):
     y_neg = 1 - y_pos
     return K.sum(y_pred_pos * y_neg) / (K.sum(y_neg) + K.epsilon())
 
-def
+
+def rnn_format_x(list_of_x):
+    x_rnn = []
+    for i in range(len(list_of_x)):
+        a0, a1, a2, a3 = np.shape(list_of_x[i])
+        x_rnn.append(np.reshape(list_of_x[i], (a0, a2, a1*a3)))
+    return x_rnn
+
+
+def rnn_format_y(list_of_y):
+    label_encoder = LabelEncoder()
+    onehot_encoder = OneHotEncoder(sparse=False)
+    y_rnn = []
+    for i in range(len(list_of_y)):
+        integer_encoded = label_encoder.fit_transform(list_of_y[i])
+        integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+        y_rnn.append(onehot_encoder.fit_transform(integer_encoded))
+    return y_rnn
