a6ed0d8e67bf64e64d66e8bc34e6fd4f079ba3ef
Alan <alanzhou93@gmail.com>
2019-04-26 02:05:31 -0400

    model updates

--- RNN/rnn_models.py
+++ RNN/rnn_models.py
@@ -18,34 +18,36 @@ def train_basic_gru(X_train, y_train, X_val, y_val, params):
     hist = model.fit(X_train, y_train, batch_size=params['batch_size'], epochs=params['epochs'],
                      validation_data=(X_val, y_val))
 
-    return hist,model
+    return hist, model
 
 
 def train_basic_lstm(X_train, y_train, X_val, y_val, params):
     model = Sequential()
     _, n_steps, n_features = np.shape(X_train)
-    model.add(LSTM(50, return_sequences=True, input_shape=(n_steps, n_features)))
-    model.add(LSTM(50))
-    model.add(Dense(50, activation='relu'))
+    for i in range(params['n_stacks']-1):
+        model.add(LSTM(params['rnn_hidden_units'], return_sequences=True, input_shape=(n_steps, n_features)))
+    model.add(LSTM(params['rnn_hidden_units']))
+    model.add(Dense(params['fc_hidden_size'], activation='relu'))
     model.add(Dense(2, activation='softmax'))
     model.compile(optimizer='adam', loss='binary_crossentropy',
                   metrics=['accuracy', utils.true_positive, utils.false_positive])
     hist = model.fit(X_train, y_train, batch_size=params['batch_size'], epochs=params['epochs'],
                      validation_data=(X_val, y_val))
 
-    return hist,model
+    return hist, model
 
 
 def train_basic_rnn(X_train, y_train, X_val, y_val, params):
     model = Sequential()
     _, n_steps, n_features = np.shape(X_train)
-    model.add(RNN(50, return_sequences=True, input_shape=(n_steps, n_features)))
-    model.add(RNN(50))
-    model.add(Dense(50, activation='relu'))
+    for i in range(params['n_stacks']-1):
+        model.add(RNN(params['rnn_hidden_units'], return_sequences=True, input_shape=(n_steps, n_features)))
+    model.add(RNN(params['rnn_hidden_units']))
+    model.add(Dense(params['fc_hidden_size'], activation='relu'))
     model.add(Dense(2, activation='softmax'))
     model.compile(optimizer='adam', loss='binary_crossentropy',
                   metrics=['accuracy', utils.true_positive, utils.false_positive])
     hist = model.fit(X_train, y_train, batch_size=params['batch_size'], epochs=params['epochs'],
                      validation_data=(X_val, y_val))
 
-    return hist,model
+    return hist, model
