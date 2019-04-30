47c73b334787c8ffc55308867b30399d867d4a22
alanzhou93 <alanzhou93@gmail.com>
2019-04-30 01:26:46 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='substorm-detection',
+      version='0.1',
+      description='Substorm Detection',
+      url='http://github.com/alanzhou93/bayesian-particle-tracking',
+      author='Greg Starr, Alan Zhou',
+      author_email='gregstarr2@gmail.com, alan.xiang.zhou@gmail.com',
+      license='General Public License',
+      packages=['substorm-detection'],
+      zip_safe=False)
